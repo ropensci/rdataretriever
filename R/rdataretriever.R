2f7eb9b2ddab8e9f95f497d2425fd52c2e42add3
Pakillo <f.rodriguez.sanc@gmail.com>
2017-09-06 15:46:11 +0200

    add Windows paths only in Windows systems

--- R/rdataretriever.R
+++ R/rdataretriever.R
@@ -309,16 +309,19 @@ check_for_retriever = function(...) {
                             '/miniconda3/bin',
                             '/miniconda2/bin')
         for (i in possible_pathes) {
-            Sys.setenv(PATH = paste(Sys.getenv('PATH'), ';', home_dir, i, sep = ''))
+            Sys.setenv(PATH = paste(Sys.getenv('PATH'), ':', home_dir, i, sep = ''))
         }
 
-        ## Other very likely pathes for Windows users:
-        more_possible_pathes = c('C:\\',
-                                 'C:\\Program files\\',
-                                 'C:\\Program files (x86)\\')
-        for (i in more_possible_pathes) {
-          Sys.setenv(PATH = paste(Sys.getenv('PATH'), ';', i, 'DataRetriever', sep = ''))
+        # paths on Windows if installed using executable:
+        if (Sys.info()[['sysname']] == "Windows") {
+          more_win_paths = c('C:\\',
+                             'C:\\Program files\\',
+                             'C:\\Program files (x86)\\')
+          for (i in more_win_paths) {
+            Sys.setenv(PATH = paste(Sys.getenv('PATH'), ';', i, 'DataRetriever', sep = ''))
+          }
         }
+
     }
 
     retriever_path = Sys.which('retriever')
