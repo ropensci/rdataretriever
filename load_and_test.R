74379cf0c5b83147bee169c9aa6fe4edb218ba1b
henrykironde <henrykironde@gmail.com>
2019-01-16 16:57:08 -0500

    A failure on tests should exit with non 0.
    testthat will enforce failure on travis if
    run with the check and progress flags.
    
    ```R
    test_dir("path_to_ tests_testthat",
    	reporter = c("check", "progress"))
    ```

--- load_and_test.R
+++ load_and_test.R
@@ -1,13 +1,13 @@
-# Install pacman if it isn't already installed
-if ("pacman" %in% rownames(installed.packages()) == FALSE) install.packages("pacman")
-
-
+# Used for continous testing platform
 # Install analysis packages using pacman
 # Pacman will load the packages and install
 # the packaes not available
+
+# Install pacman if it isn't already installed
+if ("pacman" %in% rownames(installed.packages()) == FALSE) install.packages("pacman")
+
 pacman::p_load(devtools, RCurl, readr, rmarkdown, testthat, tidyverse, DBI, RPostgreSQL,
                RSQLite, reticulate, devtools, RMariaDB)
 
 install.packages(".", repos = NULL, type="source")
-# devtools::test(reporter = c("summary", "fail"))
-devtools::test()
+test_dir("tests/testthat", reporter = c("check", "progress"))
