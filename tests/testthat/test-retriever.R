ropensci/rdataretriever

commit ab9cf4fc426ea1f25ba0c3edda047ef6a07e3544

Add get_version function
Returns the version of the Data Retriever

DESCRIPTION
@@ -48,4 +48,4 @@ Suggests:
 SystemRequirements: Python (>= 3.0) and Python headers and libraries
 License: MIT + file LICENSE
 LazyData: true
-RoxygenNote: 7.1.0
+RoxygenNote: 7.1.1

NAMESPACE
@@ -12,6 +12,7 @@ export(get_dataset_names_upstream)
 export(get_script_citation)
 export(get_script_upstream)
 export(get_updates)
+export(get_version)
 export(install)
 export(install_csv)
 export(install_json)

R/rdataretriever.R
@@ -141,6 +141,17 @@ get_script_upstream <- function(dataset, repo = "") {
   }
 }
 
+#' Get retriever version
+#' @return returns a string with the version information
+#' @examples
+#' \donttest{
+#' rdataretriever::get_version()
+#' }
+#' @importFrom reticulate import r_to_py
+#' @export
+get_version <- function() {
+  retriever$"__version__"
+}
 
 #' Fetch a dataset via the Data Retriever
 #'

tests/testthat/test-retriever.R
@@ -192,3 +192,11 @@ test_that("Citation of scripts", {
                "S. K. Morgan Ernest, Thomas J. Valone, and James H. Brown. 2009. Long-term monitoring and experimental manipulation of a Chihuahuan Desert ecosystem near Portal, Arizona, USA. Ecology 90:1708.",
                fixed = TRUE)
 })
+
+test_that("Retriever version info", {
+  skip_if_no_retriever()
+  retriever_verion <- rdataretriever::get_version()
+  url2fetch <- "https://raw.githubusercontent.com/weecology/retriever/master/retriever/_version.py"
+  url_content <- getURL(url2fetch)
+  expect_true(grepl(retriever_verion[1],url_content, fixed = TRUE))
+})

man/get_version.Rd
@@ -1,0 +1,19 @@
+% Generated by roxygen2: do not edit by hand
+% Please edit documentation in R/rdataretriever.R
+\name{get_version}
+\alias{get_version}
+\title{Get retriever version}
+\usage{
+get_version()
+}
+\value{
+returns a string with the version information
+}
+\description{
+Get retriever version
+}
+\examples{
+\donttest{
+rdataretriever::get_version()
+}
+}

commit c8c37f99c1fc40e1f2b7ae3d57bb6b2b2ec0c0ba

Check that retriever is sufficiently up-to-date
Adds a minimum retriever version to the DESCRIPTION and checks this
version against the currently installed retriever version to ensure
compatibility.Also fixes a minor issue with local testing introduced
in ab9cf4fc426ea1f25ba0c3edda047ef6a07e3544 since by adding RCurl
as a dependency.

DESCRIPTION
@@ -44,8 +44,10 @@ Suggests:
     DBI,
     devtools,
     RSQLite,
-    RPostgreSQL
-SystemRequirements: Python (>= 3.0) and Python headers and libraries
+    RPostgreSQL,
+    RCurl,
+    semver
+SystemRequirements: Python (>= 3.0), retriever (>= 2.4.1) (version must be listed to patch to allow parsing)
 License: MIT + file LICENSE
 LazyData: true
 RoxygenNote: 7.1.1

NAMESPACE
@@ -24,6 +24,8 @@ export(install_sqlite)
 export(install_xml)
 export(reload_scripts)
 export(reset)
+export(retriever_meets_min_version)
 export(use_RetrieverPath)
 importFrom(reticulate,import)
 importFrom(reticulate,r_to_py)
+importFrom(semver,parse_version)

R/rdataretriever.R
@@ -142,6 +142,7 @@ get_script_upstream <- function(dataset, repo = "") {
 }
 
 #' Get Data Retriever version
+#' @param clean boolean return cleaned version appropriate for semver
 #' @return returns a string with the version information
 #' @examples
 #' \donttest{
@@ -149,8 +150,44 @@ get_script_upstream <- function(dataset, repo = "") {
 #' }
 #' @importFrom reticulate import r_to_py
 #' @export
-data_retriever_version <- function() {
-  retriever$"__version__"
+data_retriever_version <- function(clean = TRUE) {
+  raw_version <- retriever$"__version__"
+  if (clean) {
+    no_dev_version <- gsub(".dev", "", raw_version)
+    clean_version <- gsub("v", "", no_dev_version)
+    return(clean_version)
+  } else {
+    return(raw_version)
+  }
+}
+
+#' Check Data Retriever version satisfies requirements
+#' @return boolean
+#' @examples
+#' \donttest{
+#' rdataretriever::check_data_retriever_version()
+#' }
+#' @importFrom reticulate import r_to_py
+#' @importFrom semver parse_version
+#' @export
+retriever_meets_min_version <- function() {
+  retriever_version <- data_retriever_version()
+  retriever_semver <- parse_version(retriever_version)
+  sys_req_raw <- packageDescription('rdataretriever', fields = "SystemRequirements")
+  regex_pattern <- "retriever \\([>=< ]*([0-9.]*)\\)"
+  matches <- regexec(regex_pattern, sys_req_raw)
+  min_version <- regmatches(sys_req_raw, matches)[[1]][2]
+  min_semver <- parse_version(min_version)
+  if (retriever_semver < min_semver) {
+    warning("\nNewer version of retriever required.\n\n",
+      "This version of rdataretriever requires a newer version of the retriever package.\n",
+      "Please upgrade the Python retriever package to at least ", min_version, "\n",
+      "You can do this using Python or by using the R command:\n",
+      "> reticulate::py_install('retriever')")
+    return(FALSE)
+  } else {
+    return(TRUE)
+  }
 }
 
 #' Fetch a dataset via the Data Retriever
@@ -726,4 +763,5 @@ retriever <- NULL
 .onLoad <- function(libname, pkgname) {
   ## assignment in parent environment!
   retriever <<- reticulate::import("retriever", delay_load = TRUE)
+  retriever_meets_min_version()
 }

man/data_retriever_version.Rd
@@ -4,7 +4,10 @@
 \alias{data_retriever_version}
 \title{Get Data Retriever version}
 \usage{
-data_retriever_version()
+data_retriever_version(clean = TRUE)
+}
+\arguments{
+\item{clean}{boolean return cleaned version appropriate for semver}
 }
 \value{
 returns a string with the version information

man/retriever_meets_min_version.Rd
@@ -1,0 +1,19 @@
+% Generated by roxygen2: do not edit by hand
+% Please edit documentation in R/rdataretriever.R
+\name{retriever_meets_min_version}
+\alias{retriever_meets_min_version}
+\title{Check Data Retriever version satisfies requirements}
+\usage{
+retriever_meets_min_version()
+}
+\value{
+boolean
+}
+\description{
+Check Data Retriever version satisfies requirements
+}
+\examples{
+\donttest{
+rdataretriever::check_data_retriever_version()
+}
+}

load_and_test.R
@@ -10,7 +10,8 @@ if (Sys.getenv("IN_DOCKER") == "true") {
   suppressMessages(
     pacman::p_load(devtools, RCurl, readr, rmarkdown,
                    testthat, tidyverse, DBI, RPostgreSQL,
-                   RSQLite, reticulate, devtools, RMariaDB)
+                   RSQLite, reticulate, devtools, RMariaDB,
+                   semver, RCurl)
   )
   
   install.packages(".", repos = NULL, type="source")

tests/testthat/test-retriever.R
@@ -195,8 +195,13 @@ test_that("Citation of scripts", {
 
 test_that("Data Retriever version info", {
   skip_if_no_retriever()
-  retriever_verion <- rdataretriever::data_retriever_version()
+  retriever_version <- rdataretriever::data_retriever_version(clean = FALSE)
   url2fetch <- "https://raw.githubusercontent.com/weecology/retriever/master/retriever/_version.py"
-  url_content <- getURL(url2fetch)
-  expect_true(grepl(retriever_verion[1], url_content, fixed = TRUE))
+  url_content <- RCurl::getURL(url2fetch)
+  expect_true(grepl(retriever_version[1], url_content, fixed = TRUE))
+})
+
+test_that("Data Retriever meets minimum version", {
+  skip_if_no_retriever()
+  expect_true(rdataretriever::retriever_meets_min_version())
 })